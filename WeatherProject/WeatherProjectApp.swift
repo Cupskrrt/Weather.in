fe9ec46241f43ae6423077ba657e2b869a8fb2e1
Cupskrrt <khanalfarizzi@hotmail.com>
2022-07-22 20:10:41 +0700

    Initial Commit

---
+++ WeatherProject/WeatherProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherProjectApp.swift
+//  WeatherProject
+//
+//  Created by Luthfi Khan on 22/07/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
