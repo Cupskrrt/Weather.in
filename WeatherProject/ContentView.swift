99bfe27b1628d8c66ccd903057f8ee59c12aea70
Cupskrrt <khanalfarizzi@hotmail.com>
2022-07-22 20:59:59 +0700

    Update ContentView.swift

--- WeatherProject/ContentView.swift
+++ WeatherProject/ContentView.swift
@@ -9,13 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Section{
+            VStack{
+                HStack{
+                    VStack{
+                        Text("27C")
+                            .font(.largeTitle)
+                            .bold()
+                        Text("Jakarta")
+                            .font(.title)
+                    }
+                    Image(systemName: "moon")
+                        .scaledToFit()
+                        .frame(width: 40, height: 40)
+                    
+                }
+                ScrollView(.horizontal){
+                    Form{
+                        Text("News nanti disini")
+                    }
+                    .padding(.all)
+                    .frame(width: 414, height: 100, alignment: .center)
+                    .scaledToFit()
+                    
+                }
+                
+                Rectangle()
+                    .frame(height: 2)
+                    .foregroundColor(.black)
+                
+                Text("Recent Earthquake")
+                
+                Rectangle()
+                    .frame(height: 2)
+                    .foregroundColor(.black)
+                
+                ScrollView{
+                    Form{
+                        Text("Buat Recent Earthquake")
+                    }
+                    .padding(.all)
+                    .frame(width: 414, height: 200, alignment: .center)
+                }
+            }
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
